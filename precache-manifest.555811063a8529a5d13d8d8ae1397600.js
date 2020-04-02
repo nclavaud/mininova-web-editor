5e188903e664d7a334f3e1d4c5fb88d52cfa2ab8
Nicolas Clavaud <nclavaud@gmail.com>
2020-04-02 13:26:24 +0200

    Updates

---
+++ precache-manifest.555811063a8529a5d13d8d8ae1397600.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f6c47daf1638ea92c83ed786b37af4",
+    "url": "/mininova-web-editor/index.html"
+  },
+  {
+    "revision": "8ffafbdc9aafb3073f51",
+    "url": "/mininova-web-editor/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9d002b34ea2735a1d5a1",
+    "url": "/mininova-web-editor/static/js/2.13d30da2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mininova-web-editor/static/js/2.13d30da2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ffafbdc9aafb3073f51",
+    "url": "/mininova-web-editor/static/js/main.31fe679b.chunk.js"
+  },
+  {
+    "revision": "2cdf69fcc1fa89fd9271",
+    "url": "/mininova-web-editor/static/js/runtime-main.ef3d85bd.js"
+  }
+]);
