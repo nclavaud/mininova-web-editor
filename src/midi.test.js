5dc871970909f55447ff2d8de7b2f3f3bf280bd4
Nicolas Clavaud <nclavaud@gmail.com>
2020-04-04 09:28:47 +0200

    Add coverage for midi message generation

---
+++ src/midi.test.js
@@ -0,0 +1,28 @@
+import { cc, nrpn, sysex } from './midi';
+
+test('build a CC message', () => {
+  const expected = [[0xB0, 13, 0]];
+  const sequence = cc(13, 0);
+
+  expect(sequence).toEqual(expected);
+});
+
+test('build a NRPN sequence', () => {
+  const expected = [[0xB0, 0x63, 0], [0xB0, 0x62, 122], [0xB0, 0x06, 47]];
+  const sequence = nrpn(0, 122, 47);
+
+  expect(sequence).toEqual(expected);
+});
+
+test('build a sysex sequence', () => {
+  const expected = [[0xF0, 0x00, 0x20, 0x29, 0x03, 0x01, 0x7F, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF7]];
+  const sequence = sysex([0x7F, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00]);
+
+  expect(sequence).toEqual(expected);
+});
+
+test('throw an invalid sysex value', () => {
+  const buildSequence = () => sysex([0x7F, 0x40, 0xF8]);
+
+  expect(buildSequence).toThrow(new RangeError('Sequence contains invalid sysex value'));
+});
