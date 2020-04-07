fb1f4f32dc8078fbebbb5dbc2d977ee7fe0b4af7
Nicolas Clavaud <nclavaud@gmail.com>
2020-04-07 23:59:06 +0200

    Refactor to handle input selection

--- src/App.js
+++ src/App.js
@@ -1,54 +1,78 @@
 import * as R from 'ramda';
 import React, { useState, useEffect } from 'react';
-import NoDeviceFound from './components/NoDeviceFound.js';
 import NotSupported from './components/NotSupported.js';
 import { cc, nrpn, sysex, PROGRAM_CHANGE } from './midi';
 
-const send = (device, messages) => {
-  messages.forEach(msg => device.send(msg));
-};
+const send = (device, msg) => device && device.send(msg);
+
+// is it really handshake?
+const sequenceHandshake = [0x7F, 0x60, 0x21, 0x00, 0x00, 0x00, 0x00];
+const sequencePreHandshake = [0x7F, 0x62, 0x01, 0x00, 0x00, 0x00, 0x00];
+const sequenceLoadCurrentPatch = [0x7F, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00];
 
 function App() {
-  const [midiSupport, setMidiSupport] = useState(true);
-  const [devices, setDevices] = useState(null);
-  const [selectedDevice, setSelectedDevice] = useState(null);
+  const [midiSupport, setMidiSupport] = useState(null);
+  const [availableInputs, setAvailableInputs] = useState(null);
+  const [availableOutputs, setAvailableOutputs] = useState(null);
+  const [input, setInput] = useState(null);
+  const [output, setOutput] = useState(null);
   const [currentPatch, setCurrentPatch] = useState(null);
 
-  const selectPatch = device => send(device, nrpn(63, 0, 1));
+  const onIncomingMidiMessage = message => {
+    if (message.data[0] === PROGRAM_CHANGE) {
+      setCurrentPatch(message.data[1]);
+    } else {
+      console.log(message);
+    }
+  };
 
-  const selectDevice = deviceId => {
-    setSelectedDevice(deviceId);
-    const device = R.find(R.propEq('id', deviceId))(devices);
+  const selectPatch = output => {
+    send(output, sysex(sequencePreHandshake));
+    setTimeout(() => send(output, sysex(sequenceHandshake)), 1000);
+    setTimeout(() => send(output, nrpn(63, 0, 1)), 2000);
+    setTimeout(() => send(output, sysex(sequenceLoadCurrentPatch)), 3000);
+  };
+
+  const selectOutput = id => {
+    const device = R.find(R.propEq('id', id))(availableOutputs);
+    setOutput(device);
     selectPatch(device);
   };
 
+  const selectInput = id => {
+    if (input) {
+      input.onmidimessage = null;
+    }
+    const device = R.find(R.propEq('id', id))(availableInputs);
+    device.onmidimessage = onIncomingMidiMessage;
+    setInput(device);
+  };
+
   useEffect(() => {
-    const onMidiMessage = message => {
-      if (message.data[0] === PROGRAM_CHANGE) {
-        setCurrentPatch(message.data[1]);
-      }
-    };
 
     const detectDevices = access => {
       console.log('Detecting devices');
 
-      const found = [];
+      const outputDevicesFound = [];
       const outputs = access.outputs.values();
-      let output = outputs.next();
-      while (!output.done) {
-        found.push(output.value);
-        output = outputs.next();
+      for (let output = outputs.next(); output && !output.done; output = outputs.next()) {
+        outputDevicesFound.push(output.value);
       }
-      setDevices(found);
+      setAvailableOutputs(outputDevicesFound);
 
+      const inputDevicesFound = [];
       const inputs = access.inputs.values();
-      const input = inputs.next().value;
-      input.onmidimessage = onMidiMessage;
+      for (let input = inputs.next(); input && !input.done; input = inputs.next()) {
+        inputDevicesFound.push(input.value);
+      }
+      setAvailableInputs(inputDevicesFound);
     };
 
     if (!navigator.requestMIDIAccess) {
       setMidiSupport(false);
       return;
+    } else {
+      setMidiSupport(true);
     }
 
     navigator.requestMIDIAccess({ sysex: true }).then(access => {
@@ -59,42 +83,47 @@ function App() {
     });
   }, []);
 
-  if (!midiSupport) {
+  if (null === midiSupport) {
+    return <p>Detecting MIDI support...</p>;
+  } else if (!midiSupport) {
     return <NotSupported />;
   }
 
-  if (null === devices) {
+  if (null === availableInputs || null === availableOutputs) {
     return <div>Detecting MIDI devices...</div>;
   }
 
-  const device = R.find(R.propEq('id', selectedDevice))(devices);
-
-  const changeOctave = () => send(device, cc(13, 0));
-  const activateArp = () => send(device, nrpn(0, 122, 47));
-  const deactivateArp = () => send(device, nrpn(0, 122, 46));
-  const loadPatch = () => send(device, sysex([0x7F, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00]));
-  const selectPrevPatch = () => send(device, nrpn(63, 0, 0));
-  const selectNextPatch = () => send(device, nrpn(63, 0, 2));
-
-  if (!devices.length) {
-    return <NoDeviceFound />;
-  }
+  const changeOctave = () => send(output, cc(13, 0));
+  const activateArp = () => send(output, nrpn(0, 122, 47));
+  const deactivateArp = () => send(output, nrpn(0, 122, 46));
+  const loadPatch = () => send(output, sysex(sequenceLoadCurrentPatch));
+  const selectPrevPatch = () => send(output, nrpn(63, 0, 0));
+  const selectNextPatch = () => send(output, nrpn(63, 0, 2));
 
   return (
     <div>
       <p>Available devices:</p>
       <ul>
-        {devices.map(device => (
+        {availableInputs.map(device => (
           <li key={device.id}>
-            {device.name}
-            {device.id === selectedDevice
+            IN {device.name}
+            {input && device.id === input.id
               ? ' (selected)'
-              : <button onClick={() => selectDevice(device.id)}>select</button>
+              : <button onClick={() => selectInput(device.id)}>select</button>
+            }
+          </li>
+        ))}
+        {availableOutputs.map(device => (
+          <li key={device.id}>
+            OUT {device.name}
+            {output && device.id === output.id
+              ? ' (selected)'
+              : <button onClick={() => selectOutput(device.id)}>select</button>
             }
           </li>
         ))}
       </ul>
-      {device && <>
+      <div>
         <h3>Oct / Arp</h3>
         <button onClick={changeOctave}>Change octave</button>
         <button onClick={activateArp}>Arp ON</button>
@@ -104,7 +133,7 @@ function App() {
         <button onClick={selectPrevPatch}>Prev patch</button>
         <button onClick={selectNextPatch}>Next patch</button>
         <button onClick={loadPatch}>Load patch</button>
-      </>}
+      </div>
     </div>
   );
 }
