e7867d92b44348d0c34b25eccbbeb0dc6b628b12
Nicolas Clavaud <nclavaud@gmail.com>
2020-04-17 21:40:01 +0200

    Updates

--- precache-manifest.0e048d5f941c51675c347f5bbf9bcb57.js
+++ precache-manifest.0e048d5f941c51675c347f5bbf9bcb57.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7bdbc864b635789d0d6bf11aa402528d",
+    "revision": "cbeaf2be9464e1219e9c871ffa3cb1f6",
     "url": "/mininova-web-editor/index.html"
   },
   {
-    "revision": "98d2afbb838980944bf5",
+    "revision": "e7ba95245c2a55cdfb7c",
     "url": "/mininova-web-editor/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "9d002b34ea2735a1d5a1",
-    "url": "/mininova-web-editor/static/js/2.13d30da2.chunk.js"
+    "revision": "5da852345acd0fdd3962",
+    "url": "/mininova-web-editor/static/js/2.04ea0ead.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/mininova-web-editor/static/js/2.13d30da2.chunk.js.LICENSE.txt"
+    "url": "/mininova-web-editor/static/js/2.04ea0ead.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "98d2afbb838980944bf5",
-    "url": "/mininova-web-editor/static/js/main.73739c5a.chunk.js"
+    "revision": "e7ba95245c2a55cdfb7c",
+    "url": "/mininova-web-editor/static/js/main.feb95ad5.chunk.js"
   },
   {
     "revision": "2cdf69fcc1fa89fd9271",
