9e21e2c439dfacafeabc423c2949155c8b5ff5a6
Nicolas Clavaud <nclavaud@gmail.com>
2020-05-05 00:18:57 +0200

    Communicate with the MiniNova via WebUSB
    Send/receive raw MIDI message over USB to/from the MiniNova using the
    WebUSB API. No need for a driver \o/

--- src/components/Devices.js
+++ src/components/Devices.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import * as R from 'ramda';
 import NotSupported from './NotSupported.js';
+import { cc } from '../midi';
+import { debugMidiMessage } from '../debug';
 
 function Devices({
   onChangeOutput,
@@ -28,6 +30,36 @@ function Devices({
     const device = R.find(R.propEq('id', id))(availableInputs);
     device.onmidimessage = onIncomingMidiMessage;
     setInput(device);
+  };
+
+  const detectUSB = async () => {
+    console.log('Detecting USB devices');
+    const device = await navigator.usb.requestDevice({
+      filters: [
+        {
+          vendorId: 0x1235,
+          productId: 0x001e,
+        }
+      ],
+    });
+    console.log(`Connected to ${device.productName}`);
+    console.log(device);
+    await device.open();
+    await device.selectConfiguration(1);
+    await device.claimInterface(0);
+    const data = cc(13, 0);
+    const [endpointIn, endpointOut] = [1, 2];
+    console.log('Sending a message to USB device');
+    device.transferOut(endpointOut, data);
+
+    const listen = async () => {
+      console.log('Waiting for incoming messages from USB device...');
+      const result = await device.transferIn(endpointIn, 64);
+      debugMidiMessage(new Uint8Array(result.data.buffer), 'USB Input: ');
+      listen();
+    };
+
+    listen();
   };
 
   useEffect(() => {
@@ -77,6 +109,7 @@ function Devices({
 
   return (
     <div className="devices">
+      <button onClick={detectUSB}>Connect to USB device</button>
       <p>Available devices:</p>
       <ul>
         {availableInputs.map(device => (
