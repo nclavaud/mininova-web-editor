2befe245a0593e40b7242c4333ff95e26ef161fb
Nicolas Clavaud <nclavaud@gmail.com>
2020-05-09 01:06:39 +0200

    Make "Load patch" work
    Loading a patch from the Mininova makes it send a big SYSEX message that
    exceeds the USB data chunk size (64 bytes). With this commit, consecutive
    USB data chunks gets merged in an internal buffer before being sent as a
    single, valid MIDI message to the app. Patches can now be loaded!

--- src/adapters/usb.ts
+++ src/adapters/usb.ts
@@ -8,14 +8,22 @@ const MININOVA_ENDPOINT_IN = 1;
 const MININOVA_ENDPOINT_OUT = 2;
 const MININOVA_BUFFER_LENGTH = 64;
 
+const isLastPartOfSysex = (message: Uint8Array) => {
+  const lastByte = message[message.byteLength - 1];
+
+  return lastByte === 0xF7;
+};
+
 class USBDeviceIO implements DeviceIO
 {
   private device: USBDevice;
   private onIncomingMidiMessage: MidiMessageFunc;
+  private incomingMessageBuffer: Uint8Array;
 
   constructor(device: USBDevice) {
     this.device = device;
     this.onIncomingMidiMessage = (message: MidiMessage) => {};
+    this.incomingMessageBuffer = new Uint8Array(0);
   }
 
   public setIncomingMidiMessageListener(onIncomingMidiMessage: MidiMessageFunc) {
@@ -28,11 +36,30 @@ class USBDeviceIO implements DeviceIO
     console.log('Waiting for incoming messages from USB device...');
     const result = await this.device.transferIn(MININOVA_ENDPOINT_IN, MININOVA_BUFFER_LENGTH);
     if (result?.data?.buffer) {
-      this.onIncomingMidiMessage(new Uint8Array(result.data.buffer));
+      const message = new Uint8Array(result.data.buffer);
+      if (message.length === MININOVA_BUFFER_LENGTH && !isLastPartOfSysex(message)) {
+        // incomplete message => store in buffer
+        this.appendToIncomingMessageBuffer(message);
+      } else if (this.incomingMessageBuffer.byteLength > 0) {
+        // end part of a message that has been stored in the buffer
+        this.appendToIncomingMessageBuffer(message);
+        this.onIncomingMidiMessage(this.incomingMessageBuffer);
+        this.incomingMessageBuffer = new Uint8Array(0);
+      } else {
+        // complete message, no buffer involved
+        this.onIncomingMidiMessage(message);
+      }
     }
 
     this.listen();
   };
+
+  private appendToIncomingMessageBuffer(message: Uint8Array) {
+    const b = new Uint8Array(this.incomingMessageBuffer.byteLength + message.byteLength);
+    b.set(this.incomingMessageBuffer, 0);
+    b.set(message, this.incomingMessageBuffer.byteLength);
+    this.incomingMessageBuffer = b;
+  }
 
   public send(message: Uint8Array) {
     this.device.transferOut(MININOVA_ENDPOINT_OUT, message);
