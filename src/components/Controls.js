68cf978c16f6cbf35d3e57e1296c1b0fe4d42bc8
Nicolas Clavaud <nclavaud@gmail.com>
2020-05-16 11:43:23 +0200

    Randomize all the oscillators!

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { cc, nrpn } from '../midi';
 import { controls, loadPatch } from '../mininova';
 import Oscillator from './Oscillator';
 import Control from './Control';
+import { patchControlChanged } from '../redux/patch';
+
+const randomInt = ([min, max]) => Math.floor(Math.random() * (max + 1 - min)) + min;
 
 function Controls({ currentPatch, emit }) {
+  const dispatch = useDispatch();
+
   const changeOctave = () => emit(cc(13, 0));
   const activateArp = () => emit(nrpn(0, 122, 47));
   const deactivateArp = () => emit(nrpn(0, 122, 46));
   const selectPrevPatch = () => emit(nrpn(63, 0, 0));
   const selectNextPatch = () => emit(nrpn(63, 0, 2));
+  const randomize = () => {
+    for (let [controlId, control] of Object.entries(controls)) {
+      if (!controlId.startsWith('osc-')) {
+        continue;
+      }
+      if (!control.range) {
+        continue;
+      }
+      const value = randomInt(control.range);
+      emit(control.msg(value));
+      dispatch(patchControlChanged(controlId, value));
+    }
+  };
 
   return (
     <div className="controls">
@@ -29,6 +48,7 @@ function Controls({ currentPatch, emit }) {
       <button onClick={activateArp}>Arp ON</button>
       <button onClick={deactivateArp}>Arp OFF</button>
       <h3>Oscillators</h3>
+      <button onClick={randomize}>Randomize</button>
       <div className="oscillators">
         {[1, 2, 3].map(i => (
           <Oscillator
