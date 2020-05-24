4cbd4eb19438eb4ff0e5d3e7fd56c6721c8644de
Nicolas Clavaud <nclavaud@gmail.com>
2020-05-24 10:48:45 +0200

    Add more osc/mixer controls

--- src/components/Oscillators.js
+++ src/components/Oscillators.js
@@ -17,7 +17,18 @@ function Oscillators({ emit }) {
       </div>
       <div>
         <h4>Common Oscillator parameters</h4>
-        {['osc-vibrato-depth', 'osc-vibrato-speed', 'osc-drift', 'osc-phase'].map(id => (
+        {['osc-vibrato-depth', 'osc-vibrato-speed', 'osc-drift', 'osc-phase', 'osc-fixed-note'].map(id => (
+          <Control
+            key={id}
+            id={id}
+            control={controls[id]}
+            emit={emit}
+          />
+        ))}
+      </div>
+      <div>
+        <h4>Mixer</h4>
+        {['osc-1-level', 'osc-2-level', 'ring-mod-level-1-3', 'ring-mod-level-2-3', 'noise-level', 'noise-type'].map(id => (
           <Control
             key={id}
             id={id}
