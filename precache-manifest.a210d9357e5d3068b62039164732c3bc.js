0eceac20b19edbc6295fc52b50cd8dfa41786c2c
Nicolas Clavaud <nclavaud@gmail.com>
2020-05-27 23:22:47 +0200

    Updates

--- precache-manifest.a210d9357e5d3068b62039164732c3bc.js
+++ precache-manifest.a210d9357e5d3068b62039164732c3bc.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8c846c6299e265649d20f382ae6f53dc",
+    "revision": "b146f0ed90ef38a69e6876b0f5795d7e",
     "url": "/mininova-web-editor/index.html"
   },
   {
-    "revision": "85de6aec19931a0dc948",
+    "revision": "82872ba6e15ee315eb90",
     "url": "/mininova-web-editor/static/css/main.3bb17a14.chunk.css"
   },
   {
-    "revision": "05c888164196ccc46291",
-    "url": "/mininova-web-editor/static/js/2.ed85b7ed.chunk.js"
+    "revision": "84f7506ea93120e34b3d",
+    "url": "/mininova-web-editor/static/js/2.13907579.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/mininova-web-editor/static/js/2.ed85b7ed.chunk.js.LICENSE.txt"
+    "url": "/mininova-web-editor/static/js/2.13907579.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "85de6aec19931a0dc948",
-    "url": "/mininova-web-editor/static/js/main.95f6e9d4.chunk.js"
+    "revision": "82872ba6e15ee315eb90",
+    "url": "/mininova-web-editor/static/js/main.cc613e02.chunk.js"
   },
   {
     "revision": "7015e616a69026761638",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mininova-web-editor/precache-manifest.02ab3a3b8a6a9a32a8d861ab2057d2ea.js"
+  "/mininova-web-editor/precache-manifest.a210d9357e5d3068b62039164732c3bc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mininova-web-editor/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
