635d93b177aa0d235d712a6479138ab13821dc66
Nicolas Clavaud <nclavaud@gmail.com>
2025-12-21 23:19:12 +0100

    tests: Add more tests for message decoding

--- src/mininova.test.js
+++ src/mininova.test.js
@@ -10,7 +10,31 @@ test('it detects a patch', () => {
   expect(isPatch(message)).toBe(true);
 });
 
-test('it finds a control given a command', () => {
+test('it can decode a simple control change', () => {
+  const osc1SetToMaximumLevel = {
+    type: CommandType.ControlChange,
+    values: [51, 127],
+  };
+
+  const control = findControl(osc1SetToMaximumLevel);
+
+  expect(control.id).toEqual('osc-1-level');
+  expect(control.value).toEqual(127);
+});
+
+test('it can decode a control change with linear mapping', () => {
+  const preFXLevelSetTo18dB = {
+    type: CommandType.ControlChange,
+    values: [58, 82],
+  };
+
+  const control = findControl(preFXLevelSetTo18dB);
+
+  expect(control.id).toEqual('pre-fx-level');
+  expect(control.value).toEqual(18);
+});
+
+test('it can decode a NRPN message', () => {
   const tempoSetAt40 = {
     type: CommandType.NRPN,
     values: [2, 63, 0, 40],
@@ -19,9 +43,10 @@ test('it finds a control given a command', () => {
   const control = findControl(tempoSetAt40);
 
   expect(control.id).toEqual('tempo');
+  expect(control.value).toEqual(40);
 });
 
-test('it finds a control that maps to different NRPN values', () => {
+test('it can decode a message that maps to different NRPN values', () => {
   const tempoSetAt250 = {
     type: CommandType.NRPN,
     values: [2, 63, 1, 122],
@@ -30,9 +55,10 @@ test('it finds a control that maps to different NRPN values', () => {
   const control = findControl(tempoSetAt250);
 
   expect(control.id).toEqual('tempo');
+  expect(control.value).toEqual(250);
 });
 
-test('it finds a control given a command', () => {
+test('it will not return a control when value is out of range', () => {
   const invalidTempo = {
     type: CommandType.NRPN,
     values: [2, 63, 0, 0],
